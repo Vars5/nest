0ecf0c2c2ca8e05ace8b7c3aaeb9171426afbd26
VC <varun.chhabra91@gmail.com>
2012-09-03 21:53:32 +1000

    second commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,9 @@ class StaticPagesController < ApplicationController
   def home
     #@write = Jobs.find(4).apps.find(4).notes.new
     @write = Note.new
-    
+    if user_signed_in?
+      @listjobs = current_user.jobs.all
+    end
   end
   
   
