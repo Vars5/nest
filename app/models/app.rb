91911e85ad423769d224565921ca9afcd1122627
VC <varun.chhabra91@gmail.com>
2012-09-17 23:20:06 +1000

    intializer update 4

--- app/models/app.rb
+++ app/models/app.rb
@@ -13,10 +13,10 @@ class App < ActiveRecord::Base
   
   has_attached_file :resume,
     :storage        => :s3,
-    :bucket         => ENV['Jobpages'],
+    :bucket         => ENV['S3_BUCKET'],
     :s3_credentials => { :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
                          :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] }
-    
+
 
   
   has_attached_file :coverletter
