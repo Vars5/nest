1017f78396e270b277d807437fa8597faec92bee
VC <varun.chhabra91@gmail.com>
2012-09-18 13:10:00 +1000

    updated app model

--- app/models/app.rb
+++ app/models/app.rb
@@ -17,9 +17,16 @@ class App < ActiveRecord::Base
     :s3_credentials => { :access_key_id      => ENV['S3_ACCESS_KEY'],
                          :secret_access_key  => ENV['S3_SECRET_KEY'] }
 
-
-  
   has_attached_file :coverletter
+    :storage        => :s3,
+    :bucket         => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => { :access_key_id      => ENV['S3_ACCESS_KEY'],
+                         :secret_access_key  => ENV['S3_SECRET_KEY'] }
+    
   has_attached_file :transcript
-  
+    :storage        => :s3,
+    :bucket         => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => { :access_key_id      => ENV['S3_ACCESS_KEY'],
+                         :secret_access_key  => ENV['S3_SECRET_KEY'] }
+    
 end
